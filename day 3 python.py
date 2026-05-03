49b41959ab698d91932a2917dfbea36fc02956d6
Asritha-Karthikeya <asrimurakonda@gmail.com>
2026-05-03 13:19:39 +0530

    Add files via upload

---
+++ day 3 python.py
@@ -0,0 +1,16 @@
+# Arithemaatic Operators
+
+a= 15
+b = 7
+print( a + b)# Addition
+
+print(a - b)# Substraction
+
+print(a * b) # Multipliction
+
+print(a / b) # Division
+
+print(a % b) # Moduleo
+
+print(a ** b) # Exponential
+
